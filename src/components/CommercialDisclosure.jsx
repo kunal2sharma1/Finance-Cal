cd56d61ac5affde70a67d5d16dc63b4b22dc1344
kunal2sharma1 <kunal2sharma1@gmail.com>
2026-08-17 02:38:58 +0530

    Add commercial disclosure foundation

---
+++ src/components/CommercialDisclosure.jsx
@@ -0,0 +1,10 @@
+export default function CommercialDisclosure() {
+  return (
+    <aside className="commercial-disclosure" aria-label="Commercial disclosure">
+      <strong>Commercial disclosure</strong>
+      <p>
+        Some FinCalc pages may later include sponsored or affiliate links. Any commercial relationship will be clearly labeled and will not change the calculation methodology or the order of organic calculator results.
+      </p>
+    </aside>
+  )
+}
